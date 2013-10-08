402e35bd4f69a507527b0266c07b5acf80a1496a
lithium <code@hlidskialf.com>
2013-10-08 15:48:27 -0700

    Initial Commit

---
+++ leapsynth/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  leapsynth
+//
+//  Created by Wiggins on 10/8/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
